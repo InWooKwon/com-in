801a787a3a217a55bd108eb79fbfeb0048e7bcc7
leemkl <leemkl@nate.com>
2019-11-24 04:43:03 +0900

    register dup check

--- Android/app/src/main/java/com/example/comin/RegisterActivity.java
+++ Android/app/src/main/java/com/example/comin/RegisterActivity.java
@@ -16,13 +16,12 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.comin.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    User user = new User();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,90 +36,253 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText emailText = (EditText) findViewById(R.id.emailText);
 
         Button registerButton = (Button) findViewById(R.id.registerButton);
+        Button dup_idButton = (Button) findViewById(R.id.dup_idButton);
+        Button dup_nickButton = (Button) findViewById(R.id.dup_nickButton);
 
 
-        // 데이터 업로
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        dup_idButton.setOnClickListener(new Button.OnClickListener() { //id 중복확
             @Override
-            public void onClick(View v) {
-                String userID = idText.getText().toString();
-                String userPassword=passwordText.getText().toString();
-                String userName = nameText.getText().toString();
-                String userNick = nickText.getText().toString();
-                String userBirth = birthText.getText().toString();
-                String userPhone = phoneText.getText().toString();
-                String userEmail = emailText.getText().toString();
-                String userAuth = "12345";
-
+            public void onClick(View view) {
+                // TODO : click event
+                final String userID = idText.getText().toString();
                 JSONObject testjson = new JSONObject();
                 try {
-                    //edittext의 id와 pw값을 받아와 put
                     testjson.put("userID", userID);
-                    testjson.put("userPassword", userPassword);
-                    testjson.put("userName",userName);
-                    testjson.put("userNick",userNick);
-                    testjson.put("userBirth",userBirth);
-                    testjson.put("userPhone",userPhone);
-                    testjson.put("userEmail",userEmail);
-                    testjson.put("AuthKey",userAuth);
-                    String jsonString = testjson.toString(); //완성된 json 포맷
-                    Log.d("test",jsonString);
+                    testjson.put("dup", "1");
 
-                    //전송
-                    final RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
-                    final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, getString(R.string.URL) + "register",testjson, new Response.Listener<JSONObject>() {
+                    if(userID.equals("")){
+                        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                        builder.setMessage("아이디를 입력해주세요.")
+                                .setNegativeButton("다시시도", null)
+                                .create()
+                                .show();
+                    }
+                    else {
+                        //전송
+                        final RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
+                        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, getString(R.string.URL) + "register", testjson, new Response.Listener<JSONObject>() {
 
-                        //데이터 전달을 끝내고 이제 그 응답을 받을 차례입니다.
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                //받은 json형식의 응답을 받아
-                                JSONObject jsonResponse = new JSONObject(response.toString());
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    //받은 json형식의 응답을 받아
+                                    JSONObject jsonResponse = new JSONObject(response.toString());
 
-                                //key값에 따라 value값을 쪼개 받아옵니다.
-                                String result = jsonResponse.getString("success");
+                                    //key값에 따라 value값을 쪼개 받아옵니다.
+                                    Boolean result = jsonResponse.getBoolean("dup_id");
 
-                                if(result.equals("YES")){
-                                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                    builder.setMessage("회원 등록에 성공하셨습니다.")
-                                            .setPositiveButton("확인",null)
-                                            .create()
-                                            .show();
-                                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                                    finish();
-                                    RegisterActivity.this.startActivity(intent);
+                                    if (!result) {
+                                        user.setDup_id_check(userID);
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                        builder.setMessage("사용가능한 아이디 입니다.")
+                                                .setNegativeButton("확인", null)
+                                                .create()
+                                                .show();
+                                    } else {
+                                        user.setDup_id_check("");
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                        builder.setMessage("중복된 id 입니다.")
+                                                .setNegativeButton("다시 시도", null)
+                                                .create()
+                                                .show();
+
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
                                 }
-                                else{
-                                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                    builder.setMessage("회원 등록에 실하셨습니다.")
-                                            .setNegativeButton("다시 시도",null)
-                                            .create()
-                                            .show();
-
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
                             }
-                        }
-                        //서버로 데이터 전달 및 응답 받기에 실패한 경우 아래 코드가 실행됩니다.
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            error.printStackTrace();
-                        }
-                    });
-                    jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-                    requestQueue.add(jsonObjectRequest);
+                            //서버로 데이터 전달 및 응답 받기에 실패한 경우 아래 코드가 실행됩니다.
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                error.printStackTrace();
+                            }
+                        });
+                        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                        requestQueue.add(jsonObjectRequest);
+                    }
+
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
             }
+        });
+
+        dup_nickButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO : click event
+                final String userNick = nickText.getText().toString();
+                JSONObject testjson = new JSONObject();
+
+                if(userNick.equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    builder.setMessage("닉네임을 입력해주세요.")
+                            .setNegativeButton("다시시도", null)
+                            .create()
+                            .show();
+                }
+                else {
+                    try {
+                        testjson.put("userNick", userNick);
+                        testjson.put("dup", 2);
 
 
+                        //전송
+                        final RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
+                        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, getString(R.string.URL) + "register", testjson, new Response.Listener<JSONObject>() {
+
+                            //데이터 전달을 끝내고 이제 그 응답을 받을 차례입니다.
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    //받은 json형식의 응답을 받아
+                                    JSONObject jsonResponse = new JSONObject(response.toString());
+
+                                    //key값에 따라 value값을 쪼개 받아옵니다.
+                                    Boolean result = jsonResponse.getBoolean("dup_nick");
+
+                                    if (!result) {
+                                        user.setDup_nick_check(userNick);
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                        builder.setMessage("사용가능한 닉네임 입니다.")
+                                                .setNegativeButton("확인", null)
+                                                .create()
+                                                .show();
+                                    } else {
+                                        user.setDup_nick_check("");
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                        builder.setMessage("중복된 닉네임 입니다.")
+                                                .setNegativeButton("다시 시도", null)
+                                                .create()
+                                                .show();
+
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            //서버로 데이터 전달 및 응답 받기에 실패한 경우 아래 코드가 실행됩니다.
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                error.printStackTrace();
+                            }
+                        });
+                        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                        requestQueue.add(jsonObjectRequest);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         });
 
 
+            registerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String userID = idText.getText().toString();
+                    String userPassword = passwordText.getText().toString();
+                    String userName = nameText.getText().toString();
+                    String userNick = nickText.getText().toString();
+                    String userBirth = birthText.getText().toString();
+                    String userPhone = phoneText.getText().toString();
+                    String userEmail = emailText.getText().toString();
+                    String userAuth = "12345";
+
+                    if (userID.equals("") || userPassword.equals("") || userName.equals("") || userNick.equals("") || userBirth.equals("") || userPhone.equals("") || userEmail.equals("")) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                        builder.setMessage("회원 등록에 실패하셨습니다. 입력하였는지 확인해주세요")
+                                .setPositiveButton("다시시도", null)
+                                .create()
+                                .show();
+
+                    } else {
+                        if (user.getDup_id_check().equals(userID) && user.getDup_nick_check().equals(userNick)){
+
+                            JSONObject testjson = new JSONObject();
+                            try {
+                                //edittext의 id와 pw값을 받아와 put
+                                testjson.put("userID", userID);
+                                testjson.put("userPassword", userPassword);
+                                testjson.put("userName", userName);
+                                testjson.put("userNick", userNick);
+                                testjson.put("userBirth", userBirth);
+                                testjson.put("userPhone", userPhone);
+                                testjson.put("userEmail", userEmail);
+                                testjson.put("AuthKey", userAuth);
+                                testjson.put("dup", 3);
+                                String jsonString = testjson.toString(); //완성된 json 포맷
+                                Log.d("test", jsonString);
+
+                                //전송
+                                final RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
+                                final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, getString(R.string.URL) + "register", testjson, new Response.Listener<JSONObject>() {
+
+                                    //데이터 전달을 끝내고 이제 그 응답을 받을 차례입니다.
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        try {
+                                            //받은 json형식의 응답을 받아
+                                            JSONObject jsonResponse = new JSONObject(response.toString());
+
+                                            //key값에 따라 value값을 쪼개 받아옵니다.
+                                            Boolean result = jsonResponse.getBoolean("success");
+
+                                            if (result) {
+                                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                                builder.setMessage("회원 등록에 성공하셨습니다.")
+                                                        .setPositiveButton("확인", null)
+                                                        .create()
+                                                        .show();
+                                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                                finish();
+                                                RegisterActivity.this.startActivity(intent);
+                                            } else {
+                                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                                builder.setMessage("회원 등록에 실패하셨습니다.")
+                                                        .setNegativeButton("다시 시도", null)
+                                                        .create()
+                                                        .show();
+
+                                            }
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                    //서버로 데이터 전달 및 응답 받기에 실패한 경우 아래 코드가 실행됩니다.
+                                }, new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        error.printStackTrace();
+                                    }
+                                });
+                                jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                                requestQueue.add(jsonObjectRequest);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        } else{
+                            AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                            builder.setMessage("아이디 및 닉네임 중복을 확인해주세요.")
+                                    .setNegativeButton("다시 시도", null)
+                                    .create()
+                                    .show();
+
+                        }
+
+                    }
+                }
+            });
+
+
+        }
     }
-}
+
+
+
--- Server/app.js
+++ Server/app.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
     host: "127.0.0.1",
     port: 3306,
     user: "root",
-    password: "1111",
+    password: "alzlwl258",
     database: "comindb",
     multipleStatements: true
 });
@@ -87,7 +87,7 @@ app.post('/login', function (req, res, next) {
 
 //register POST
 app.post('/register', function (req, res) {
-    var approve = { 'success': 'NO' };
+    var approve = { 'success': false ,'dup_id':false,'dup_nick':false};
 
     var body = req.body;
     var id = body.userID;
@@ -98,21 +98,59 @@ app.post('/register', function (req, res) {
     var phone = body.userPhone;
     var email = body.userEmail;
     var key = body.AuthKey;
-    var command = 'INSERT INTO user (id, pw, realName, nickName, birth, email, phoneNumber, authKey) values (\'' + id + '\',\'' + password + '\',\'' + name + '\',\'' + nick + '\',\'' + birth + '\',\'' + email + '\',\'' + phone + '\',\'' + key + '\')';
-    console.log(command);
-    var query = connection.query(command, function (err, rows, fields) {
-        if (err) {
-            console.log("register fail");
-            approve.success = 'NO';
-        }
-        else {
-            console.log("Data inserted");
-            approve.success = 'YES';
-        }
+    var dup=body.dup;
+    if(dup=="1"){//중복 검사
+        var command = 'select * from user where id=\'' + id + '\'';
+        connection.query(command,function(err,rows,fields){
+            if(err){
+                console.log("register dup_id error");
+            }
+            else{
+                if(rows.length>0){
+                    approve.dup_id=true;
+                }
+                else{
+                    approve.dup_id=false;
+                }
+                res.send(approve);
+            }
+        });
+    }
+    else if(dup==2){//중복검사
+        var qry='SELECT * FROM user where nickName =\''+nick+'\'';
+        connection.query(qry,function(err,rows,fields){
+            if(err){
+                console.log("register dup_id error");
+            }
+            else{
+                if(rows.length>0){
+                    approve.dup_nick=true;
+                }
+                else{
+                    approve.dup_nick=false;
+                }
+                res.send(approve);
+            }
+        });
+    }
 
-        res.send(approve);
+    else{
+        var command = 'INSERT INTO user (id, pw, realName, nickName, birth, email, phoneNumber, authKey) values (\'' + id + '\',\'' + password + '\',\'' + name + '\',\'' + nick + '\',\'' + birth + '\',\'' + email + '\',\'' + phone + '\',\'' + key + '\')';
+        console.log(command);
+        connection.query(command, function (err, rows, fields) {
+            if (err) {
+                console.log("register fail");
+                approve.success = false;
+                res.send(approve);
+            }
+            else {
+                console.log("Data inserted");
+                approve.success = true;
+                res.send(approve);
+            }   
 
-    })
+        })
+    }
 })
 
 module.exports = router;
