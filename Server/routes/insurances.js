b6809c6c2decf24ad97fafddfd5a3d80eb415db7
unknown <hja5432@gmail.com>
2019-11-30 01:10:28 +0900

    hot보험 서버변경

--- Server/routes/insurances.js
+++ Server/routes/insurances.js
@@ -12,6 +12,21 @@ router.get('/', function (req, res) {
     });
 });
 
+router.get('/', function (req, res) {
+    var qry = "SELECT * FROM INSURANCE;" + "SELECT * FROM COVERAGE;" + "SELECT tag1 FROM boardInfo where type = 1;";
+    connection.query(qry, function (err, result, fields) {
+        var rst = {"insurances" : result[0], "coverages" : result[1], "reviewBoard" : result[2]};
+        res.json(rst);
+    });
+});
+
+router.get('/hot', function (req, res) {
+    var qry = "select insurance.idx, count(boardInfo.tag1) * score as cnt from insurance, boardInfo where boardInfo.tag1 = insurance.idx group by insurance.idx order by cnt desc;";
+    connection.query(qry, function (err, result, fields) {
+        var rst = {"hotInsurances" : result};
+        res.json(rst);
+    });
+});
 
 router.get('/:coverage', function (req, res) {
     var coverage = req.params.coverage;
